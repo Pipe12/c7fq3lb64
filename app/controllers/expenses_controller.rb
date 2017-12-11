147b01a68974e2946a01d9d9da7765a789324f6f
Andres Burbano <ciento1@hotmail.com>
2017-12-11 18:06:34 -0500

    realiza implementacion de filtros parciales

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,11 +2,11 @@ class ExpensesController < ApplicationController
   def index
   	if (params[:category_id].present? || params[:concept].present?)
   		if (params[:category_id].present? && params[:concept].present?)
-  			@expenses = Expense.where("category_id = ? AND concept = ?", params[:category_id], params[:concept])
+  			@expenses = Expense.where("category_id = ? AND concept like ?", params[:category_id], "%#{params[:concept]}%")
   		elsif (params[:category_id].present?)
   			@expenses = Expense.where("category_id = ?", params[:category_id])
   		else
-  			@expenses = Expense.where("concept = ?", params[:concept])
+  			@expenses = Expense.where("concept LIKE ?", "%#{params[:concept]}%")
   		end
   	else
   		@expenses = Expense.order("date DESC")
